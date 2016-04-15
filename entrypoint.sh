5ff6f9cf62d3b965bd9e7c1edd3ffc14f9ee897e
Tobias Florek <tob@butter.sh>
2016-04-15 09:35:58 +0200

    fix logging to stdout

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,17 @@
 
 USERDB=/etc/vsftpd/user.db
 VSFTPD_OPTIONS=${VSFTPD_OPTIONS-}
-DEFAULT_OPTIONS="-oanonymous_enable=NO -ohide_ids=YES -ouser_sub_token=USER -olocal_root=/home/vftp/USER -opam_service_name=vsftpd.virtual -ovirtual_use_local_privs=YES -ovsftpd_log_file=/var/log/vsftpd.log -oxferlog_std_format=NO -ochroot_local_user=NO -owrite_enable=YES -oguest_enable=YES"
+DEFAULT_OPTIONS="-oanonymous_enable=NO -ohide_ids=YES -ouser_sub_token=USER -olocal_root=/home/vftp/USER -opam_service_name=vsftpd.virtual -ovirtual_use_local_privs=YES -ovsftpd_log_file=/proc/1/fd/1 -oxferlog_std_format=NO -ochroot_local_user=NO -owrite_enable=YES -oguest_enable=YES"
 OVERRIDES="-obackground=NO"
 LOG_STDOUT=${LOG_STDOUT-y}
 
 main() {
     generate_userdb
-    if [ $LOG_STDOUT = y ]; then
-        rm -f /var/log/vsftpd.log
-        mkfifo /var/log/vsftpd.log
-        cat /var/log/vsftpd.log &
+    if [ ! $LOG_STDOUT = y ]; then
+        DEFAULT_OPTIONS="${DEFAULT_OPTIONS} -ovsftpd_log_file=/var/log/vsftpd.log"
     fi
-    exec /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf $DEFAULT_OPTIONS $VSFTPD_OPTIONS $OVERRIDES
+    /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf $DEFAULT_OPTIONS $VSFTPD_OPTIONS $OVERRIDES
+    wait $?
 }
 
 generate_userdb() {
